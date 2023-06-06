6061f96b199007940ad1562a43c262457651a46c
Tran Long <tranlong@Trans-MBP-2.zoi.local>
2023-06-06 18:07:52 +0700

    init project

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+// import ClassicEditor from './Editor.js';
+// import { CKEditor } from '@ckeditor/ckeditor5-react';
+import React from 'react';
+import Blog from '../blogs/Blog.js';
+
+function App() {
+  return <Blog />;
+}
+
+export default App;
